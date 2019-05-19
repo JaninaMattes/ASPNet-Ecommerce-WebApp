3c8b671484cc6f84daf6b90e4e6aa5ec99682e4d
Janina Mattes <jm105@hdm-stuttgart.de>
2019-05-19 23:36:25 +1000

    src: Update functions for Shippment Service Business Logic Layer

---
+++ WebsiteLaitBrasseur/BL/ProductSelectionBL.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebsiteLaitBrasseur.DAL;
+
+namespace WebsiteLaitBrasseur.BL
+{
+    public class ProductSelectionBL
+    {
+        ProductSelectionDAL DB = new ProductSelectionDAL();
+
+        /// <summary>
+        /// TODO Check the logic of shopping
+        /// </summary>
+        /// <param name="invoiceID"></param>
+        /// <param name="quantity"></param>
+        /// <param name="origSize"></param>
+        /// <param name="origPrice"></param>
+        /// <returns></returns>
+        public int Create(int invoiceID, int quantity, int origSize, decimal origPrice)
+        {
+            int result = 0;
+            try
+            {
+                result = DB.Insert(invoiceID, quantity, origSize, origPrice);
+            }
+            catch (Exception e)
+            {
+                e.GetBaseException();
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Get all available Sizes per product.
+        /// </summary>
+        /// <param name="invoiceID"></param>
+        /// <returns></returns>
+        public List<ProductSelectionDTO> GetProducts(int invoiceID)
+        {
+            List<ProductSelectionDTO> results = new List<ProductSelectionDTO>();
+            try
+            {
+                results = DB.FindAllPerInvoice(invoiceID);
+            }
+            catch (Exception e)
+            {
+                e.GetBaseException();
+            }
+            return results;
+        }
+
+        //TODO
+    }
+}
---
+++ WebsiteLaitBrasseur/BL/ShippmentBL.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebsiteLaitBrasseur.DAL;
+
+namespace WebsiteLaitBrasseur.BL
+{
+    public class ShippmentBL
+    {
+        ShippmentDAL DB = new ShippmentDAL();
+        
+        /// <summary>
+        /// Creates a new entry in the DB
+        /// and returns the ID.
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="deliveryTime"></param>
+        /// <param name="company"></param>
+        /// <param name="cost"></param>
+        /// <param name="status"></param>
+        /// <returns></returns>
+        public int Create(string type, int deliveryTime, string company, decimal cost, int status)
+        {
+            int result = 0;
+            try
+            {
+                result = DB.Insert(type, deliveryTime, company, cost, status);
+            }
+            catch (Exception e)
+            {
+                e.GetBaseException();
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Update the status and suspend a deliverer. 
+        /// </summary>
+        /// <param name="delivererID"></param>
+        /// <param name="status"></param>
+        /// <returns></returns>
+        public int ChangeStatus(int delivererID, int status)
+        {
+            int result = 0;
+            try
+            {
+                result = DB.Update(delivererID, status);
+            }
+            catch (Exception e)
+            {
+                e.GetBaseException();
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Find all availble services.
+        /// </summary>
+        /// <returns></returns>
+        public List<ShippmentDTO> GetPostServices()
+        {
+            List<ShippmentDTO> result = new List<ShippmentDTO>();
+            int status = 0;
+            try
+            {
+                result = DB.FindAllBy(status);
+            }
+            catch (Exception e)
+            {
+                e.GetBaseException();
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Find all currently suspendet Services.
+        /// </summary>
+        /// <returns></returns>
+        public List<ShippmentDTO> GetPostService()
+        {
+            List<ShippmentDTO> result = new List<ShippmentDTO>();
+            int status = 1;
+            try
+            {
+                result = DB.FindAllBy(status);
+            }
+            catch (Exception e)
+            {
+                e.GetBaseException();
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Find all services.
+        /// </summary>
+        /// <returns></returns>
+        public List<ShippmentDTO> GetAllPostServices()
+        {
+            List<ShippmentDTO> result = new List<ShippmentDTO>();
+            try
+            {
+                result = DB.FindAll();
+            }
+            catch (Exception e)
+            {
+                e.GetBaseException();
+            }
+            return result;
+        }
+    }
+}
