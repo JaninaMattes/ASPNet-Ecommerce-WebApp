442b933a2fe7c2095f6d62de21cdb587197d8bed
Janina Mattes <jm105@hdm-stuttgart.de>
2019-05-19 18:43:57 +1000

    src: Update Address Business Logic Layer

--- WebsiteLaitBrasseur/BL/AddressBL.cs
+++ WebsiteLaitBrasseur/BL/AddressBL.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Web;
@@ -9,6 +10,7 @@ namespace WebsiteLaitBrasseur.BL
 {
     public class AddressBL
     {
+        private AccountDAL AB = new AccountDAL();
         private AddressDAL DB = new AddressDAL();
         private CityDAL CB = new CityDAL();
 
@@ -33,6 +35,7 @@ namespace WebsiteLaitBrasseur.BL
             {
                 int cityID = CB.Insert(zipCode, cityName);
                 result = DB.Insert(cityID, streetName, streetNo, addressType);
+                Debug.Print("AddressBL: /Insert/ " + result.ToString());
             }
             else
             {
@@ -41,6 +44,75 @@ namespace WebsiteLaitBrasseur.BL
             return result;
         }
 
+        /// <summary>
+        /// Update an address and the city in the DB
+        /// Business Rules:
+        /// if failed result = 0
+        /// if successfull result = 1
+        /// if zipCode invalid result = 2
+        /// 
+        /// </summary>
+        /// <param name="email"></param>
+        /// <param name="zipCode"></param>
+        /// <param name="cityName"></param>
+        /// <param name="streetName"></param>
+        /// <param name="streetNo"></param>
+        /// <param name="addressType"></param>
+        /// <returns></returns>
+        public int UpdateAddress(string email, string zipCode, string cityName, string streetName, string streetNo, string addressType)
+        {
+            int result = 0;
+            AccountDTO customer = new AccountDTO();
+            AddressDTO address = new AddressDTO();
+            if (IsPostCodeValid(zipCode))
+            {
+                customer = AB.FindBy(email);
+                if (customer != null)
+                {
+                    address = DB.FindBy(customer.GetAddress().GetID());
+                }
+                if(address != null)
+                {
+                    CB.UpdateCity(address.GetCity().GetId(), zipCode, cityName);
+                    result = DB.UpdateAddress(address.GetID(), address.GetCity().GetId(), streetName, streetNo, addressType);
+                    Debug.Print("AddressBL: /Insert/ " + result.ToString());
+                }                
+            }
+            else
+            {
+                result = 2;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Find an address of a customer.
+        /// </summary>
+        /// <param name="accountID"></param>
+        /// <returns></returns>
+        public AddressDTO FindAddress(int accountID)
+        {
+            AddressDTO address = new AddressDTO();
+            AccountDTO customer = new AccountDTO();
+            customer = AB.FindBy(accountID);
+            if (customer != null)
+            {
+                address = DB.FindBy(customer.GetAddress().GetID());
+                Debug.Print("AddressBL: /FindAddress/ " + address.ToString());
+            }
+            else
+            {
+                throw new EmptyRowException("No result found.");
+            }
+            
+            return address;
+        }
+
+        /// <summary>
+        /// Check if the post code is any valid.
+        /// </summary>
+        /// <param name="postCode"></param>
+        /// <returns></returns>
         private bool IsPostCodeValid(string postCode)
         {
             Regex postCodeValidation = new Regex(@"^[0234567]\d{4}$");
